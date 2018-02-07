5022b22326663a07f9d672e79c007615338f78b9
Fdd <Fdd@10.89.201.88>
2018-02-08 00:08:12 +0800

    Added a congratulation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -4,6 +4,7 @@ public class Lab1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Lab 1: Introduction to Git and GitHub");
+		System.out.println("Congratulation! You learnt the Git Basics."); 
 	}
 
 }
